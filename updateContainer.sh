ba6996a826a7df3acfb100301df7ff111f5b5e14
gr0vity <gr0vity.dev@gmail.com>
2018-04-18 20:05:45 +0200

    Add -m parameter to pull master branch instead of latest release

--- updateContainer.sh
+++ updateContainer.sh
@@ -1,29 +1,34 @@
-dockerName=$(docker ps -a --format "{{.Image}}\t{{.Names}}" | grep "nanocurrency/nano" | awk '{ print $2}')
-flgUpdate=false
 currentDate=$(date +"%d.%m.%Y %H:%M:%S")
-while getopts "n:u" OPTION
+flgUpdate=false
+dockerImage=nanocurrency/nano
+dockerName=$(docker ps -a --format "{{.Image}}\t{{.Names}}" | grep "nanocurrency/nano" | awk '{ print $2}')
+
+while getopts "n:um" OPTION
 do
         case $OPTION in
                 n)
-                        printf "ContainerName set to $OPTARG"
+                        printf "ContainerName set to $OPTARG \n"
                         dockerName=$OPTARG
                         ;;
                 u)
-                        printf "Enabled update forcefully"
+                        printf "Enabled update forcefully \n"
                         flgUpdate=true
-                        ;;
+                      ;;
+				m)
+						printf "get master branch instead of official release \n"
+						dockerImage=nanocurrency/nano:master
+						;;
                 \?)
                         printf "[-n {dockerConatinerName}] : update this container if outdated \n[-u] : force update and remove old container\n"
                         exit
                         ;;
         esac
 done
-#echo "DockerName : $dockerName \n Force Update: $forceUpdate"
 
-currentDockerId=$(docker ps -a --filter "name=$dockerName" -q | xargs)
+dockerId=$(docker ps -a --filter "name=$dockerName" -q | xargs)
 if  docker pull nanocurrency/nano | grep -q 'Image is up to date' ;
         then
-         echo "No updates available for $dockerName (Container Id: $currentDockerId)"
+         echo "No updates available for $dockerName (Container Id: $dockerId)\n"
         else
          flgUpdate=true
 fi
@@ -31,10 +36,12 @@ fi
 if $flgUpdate -eq true ;
         then
          echo "Docker will be updated..."
-          docker stop $currentDockerId
-          docker rm $currentDockerId
-          docker run -d -p 7075:7075/udp -p 7075:7075 -p [::1]:7076:7076 -v /home/nanonode/:/root --name $dockerName --restart unless-stopped nanocurrency/nano
-          echo "$currentDate Docker Container ($dockerName) updated" >> /var/log/cron_nano_docker_updates.txt
+          docker stop $dockerId
+          docker rm $dockerId
+          if ! docker run -d -p 7075:7075/udp -p 7075:7075 -p [::1]:7076:7076 -v /home/nanonode/:/root --name $dockerName --restart unless-stopped $dockerImage ; then
+			   docker run -d -p 7075:7075/udp -p 7075:7075 -p 127.0.0.1:7076:7076 -v /home/nanonode/:/root --name $dockerName --restart unless-stopped $dockerImage
+		  fi
+		  echo "$currentDate Docker Container ($dockerName) updated with Image ($dockerImage)" >> /var/log/cron_nano_docker_updates.txt
 else
           echo "$currentDate most recent version already installed" >> /var/log/cron_nano_docker_no_updates.txt
 fi
